c1bfb1e1c1c18564f7f16805b55fc65885236879
daedalus31 <mat.marczykowski@gmail.com>
2018-05-16 16:01:16 +0200

    zadanie 3 dodane

---
+++ io/clean_emails.py
@@ -0,0 +1,28 @@
+import re
+import sys
+
+
+def is_valid_email(email: str):
+    email_regex = re.compile('[^@]+@[^@]+\.[^@]+')
+    return email_regex.match(email)
+
+
+def clean_emails_set(input_file):
+    result = []
+    with open(input_file) as inp:
+        for email in inp:
+            email = email.strip().lower()
+            if is_valid_email(email):
+                result.append(email)
+    return set(result)
+
+
+def write_clean_list(emails: set, output_file: str):
+    with open(output_file, 'w') as out:
+        out.writelines([e + '\n' for e in sorted(emails)])
+
+
+if __name__ == '__main__':
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    write_clean_list(clean_emails_set(input_file), output_file)
