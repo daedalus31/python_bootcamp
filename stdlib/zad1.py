264b6b4a3b934463191ed7dafed06830c794f531
daedalus31 <mat.marczykowski@gmail.com>
2018-05-17 09:23:52 +0200

    refaktor zad. 1 z stdlib - wyjątek sprawdzany tylko tam, gdzie trzeba

--- stdlib/zad1.py
+++ stdlib/zad1.py
@@ -7,23 +7,23 @@ class EmployeeDataBase:
         self._employees = []
 
     def load_employees(self):
-        with open(self._data_file) as data:
-            self._employees = json.load(data)
-
-    def print_employees(self):
         try:
-            self.load_employees()
-        except FileNotFoundError:
-            print(f'Plik {self._data_file} nie istnieje!')
-        for i, e in enumerate(self._employees, start=1):
-            print(
-                f'[{i}] {e["firstname"]} {e["lastname"]} - rok: {e["birthyear"]}, pensja: {e["salary"]:.2f}')
-
-    def add_employee(self, employee_dict):
-        try:
-            self.load_employees()
+            with open(self._data_file) as data:
+                self._employees = json.load(data)
         except FileNotFoundError:
             pass
+
+    def print_employees(self):
+        self.load_employees()
+        if not self._employees:
+            print(f'Plik {self._data_file} nie istnieje lub jest pusty!')
+        else:
+            for i, e in enumerate(self._employees, start=1):
+                print(
+                    f'[{i}] {e["firstname"]} {e["lastname"]} - rok: {e["birthyear"]}, pensja: {e["salary"]:.2f}')
+
+    def add_employee(self, employee_dict):
+        self.load_employees()
         self._employees.append(employee_dict)
         with open(self._data_file, 'w') as data:
             json.dump(self._employees, data)
